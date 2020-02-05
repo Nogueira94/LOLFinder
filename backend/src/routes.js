0b00261bd2984e1d391023aa581718972b827c3c
Guilherme <guilherme.nogueira11@fatec.sp.gov.br>
2020-02-05 00:02:35 -0300

    Testando API do LoL

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,21 @@ const DevController = require('./controllers/DevController'); // importando os d
 const SearchController = require('./controllers/SearchController'); // importando os dados do DevController para utilizar no post
 const routes = Router();
 
-routes.get('/devs', DevController.index);
-routes.post('/devs', DevController.store);
+routes.post('/lol',async (request,response) => {
 
-routes.get('/search', SearchController.index);
+    var apikey = '?api_key=' + 'RGAPI-f717f760-d027-477b-8e14-f2d632ebd6d4';
+    
+    const {name} = request.body;
+
+    const apiResponse = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}+${apikey}`)
+
+    console.log(apiResponse.data)
+
+})
+
+//routes.get('/devs', DevController.index);
+//routes.post('/devs', DevController.store);
+
+//routes.get('/search', SearchController.index);
 
 module.exports = routes; // exportando o objeto Routes
