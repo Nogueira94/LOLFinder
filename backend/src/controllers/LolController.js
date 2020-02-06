52edfdfbc42d6c8ece2094b5867f5a2f6b1e5e2c
Guilherme <guilherme.nogueira11@fatec.sp.gov.br>
2020-02-06 00:05:40 -0300

    Resposta da API - OK

--- backend/src/controllers/LolController.js
+++ backend/src/controllers/LolController.js
@@ -23,26 +23,44 @@ module.exports = {
 
             const apiResponse2 = await axios.get(`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${userId}${apikey}`);
 
+            var unranked = apiResponse2.data.rank;
+
             console.log(apiResponse2.data)
 
             const location = {
             type: 'Point',
             coordinates: [longitude, latitude] 
             }
-
-            var newObject = {
-                "name": apiResponse.data.name,
-                "profileIconId": apiResponse.data.profileIconId,
-                "summonerLevel": apiResponse.data.summonerLevel,
-                "id": apiResponse.data.id,
-                "wins": apiResponse2.data[0].wins,
-                "losses": apiResponse2.data[0].losses,
-                "rank": apiResponse2.data[0].rank,
-                "tier": apiResponse2.data[0].tier,
-                "mainRole": request.body.mainRole,
-                "secRole" : request.body.secRole,
-                "location": location
-            }
+            
+            if (apiResponse2.data != "") {
+                var newObject = {
+                    "name": apiResponse.data.name,
+                    "profileIconId": apiResponse.data.profileIconId,
+                    "summonerLevel": apiResponse.data.summonerLevel,
+                    "id": apiResponse.data.id,
+                    "wins": apiResponse2.data[0].wins,
+                    "losses": apiResponse2.data[0].losses,
+                    "rank": apiResponse2.data[0].rank,
+                    "tier": apiResponse2.data[0].tier,
+                    "mainRole": request.body.mainRole,
+                    "secRole" : request.body.secRole,
+                    "location": location
+                }
+            } else {
+                var newObject = {
+                    "name": apiResponse.data.name,
+                    "profileIconId": apiResponse.data.profileIconId,
+                    "summonerLevel": apiResponse.data.summonerLevel,
+                    "id": apiResponse.data.id,
+                    "wins": null,
+                    "losses": null,
+                    "rank": "Unranked",
+                    "tier": null,
+                    "mainRole": request.body.mainRole,
+                    "secRole" : request.body.secRole,
+                    "location": location
+                }
+            }         
 
             console.log(newObject)
         
